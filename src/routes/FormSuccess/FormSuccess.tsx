17d0b6db855e08f3e74abee5e373c6165f2e1864
Maxisthemoose <oriontothemax@gmail.com>
2024-09-16 22:45:20 -0700

    form success page template

---
+++ src/routes/FormSuccess/FormSuccess.tsx
@@ -0,0 +1,9 @@
+import "./FormSuccess.css";
+
+export default function Success() {
+  return (
+    <div className="success-page">
+      {/* TODO: Success page after contact form. Thanks for submission + navigation back to home/other pages */}
+    </div>
+  )
+}
