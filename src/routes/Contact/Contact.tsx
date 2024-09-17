ea933c60eb18068b1fc2e478201911053d2e3d3a
Maxisthemoose <oriontothemax@gmail.com>
2024-09-17 11:28:03 -0700

    forgot to add action='/success'

--- src/routes/Contact/Contact.tsx
+++ src/routes/Contact/Contact.tsx
@@ -36,7 +36,7 @@ export default function Contact() {
         <div className="contact-form-wrapper">
           <h2 className="contact-us-header">Contact Us</h2>
 
-          <form method="post" className="contact-form" name="contact">
+          <form method="post" action="/success" className="contact-form" name="contact">
 
             <input type="hidden" name="form-name" value="contact" />
             <div className="name-container">
