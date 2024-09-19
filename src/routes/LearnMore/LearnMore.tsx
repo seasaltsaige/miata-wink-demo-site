08f7aff23db2968a8ee700ca091bd62535f18521
seasaltsaige <oriontothemax@gmail.com>
2024-09-18 17:48:55 -0700

    learn more page started

--- src/routes/LearnMore/LearnMore.tsx
+++ src/routes/LearnMore/LearnMore.tsx
@@ -1,8 +1,28 @@
 import "./LearnMore.css";
 
-
 export default function LearnMore() {
   return <div className="learn-more-page">
+    <div className="content-container">
 
+      <h1 className="wink-mod-header">
+        What is a 'Wink Mod?'
+      </h1>
+      <p className="wink-mod-description">
+        A 'Wink Mod' refers to the ability to wink your Miata's headlights. This means 'blinking' one headlight instead of both.
+        Often times people also include the ability to move the headlights to an arbitrary position, rather than all the way in one direction. This is referred to as 'Sleepy Eye.'
+        Winking is often done by disconnecting one of the headlight connectors, to only allow one headlight to operate normally.
+        While this does achieve the goal of 'Winking' the headlights, this is both inefficient, and dangerous.
+      </p>
+
+      <h1 className="wink-mod-header">
+        What is our product?
+      </h1>
+
+      <p className="wink-mod-description">
+        Our Wink Mod achieves that classic winking ability, in a much safer, easily reversible manner. Our product is designed to plug directly into your Miata's OEM headlight connectors.
+
+      </p>
+
+    </div>
   </div>
 }
